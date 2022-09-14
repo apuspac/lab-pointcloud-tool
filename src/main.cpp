e9776d2c6821184cca09d923c7d7c27f57b527ed
apuspac <penginstar1299@gmail.com>
2022-09-14 20:43:37 +0900

    feature #3 add process to calc rotation matrix of R

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/opencv.hpp>
 #include <eigen3/Core>
 #include <eigen3/SVD>
+#include <eigen3/LU>
 
 
 Eigen::Matrix3d calc_correlation_C(
@@ -47,7 +48,28 @@ Eigen::Matrix3d calc_correlation_C(
     return correlation_C;    
 }
 
-void SVD_correlation_C(Eigen::Matrix3d matrix_C)
+Eigen::Matrix3d calc_rotation_R(Eigen::Matrix3d correlation_C)
+{
+    // 特異値分解
+    Eigen::JacobiSVD<Eigen::Matrix3d> SVD(correlation_C, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::Matrix3d matrix_V, matrix_U,  matrix_R;
+
+    matrix_U = SVD.matrixU();
+    matrix_V = SVD.matrixV();
+
+    // det(V UT) 計算結果は 1か-1
+    double det_VU = (matrix_V * matrix_U.transpose()).determinant();
+
+    //対角行列
+    Eigen::DiagonalMatrix<double, 3> matrix_Diag = {1.0, 1.0, det_VU};
+
+    // 回転行列Rの最大化の式
+    matrix_R = matrix_V * matrix_Diag * matrix_U.transpose();
+    
+    return matrix_R;
+}
+
+void SVD_test(Eigen::Matrix3d matrix_C)
 {
     Eigen::JacobiSVD<Eigen::Matrix3d> SVD(matrix_C, Eigen::ComputeFullU | Eigen::ComputeFullV);
 
@@ -70,16 +92,35 @@ int main()
 {
     // 仮データ準備
     std::vector<Eigen::Vector3d> x, x_p;
-    
-    Eigen::Vector3d a = {1.0, 2.0, 3.0};
-    x.push_back(a);
-    x.push_back(a);
-    x.push_back(a);
 
-    Eigen::Vector3d b = {2.0, 2.0, 3.0};
-    x_p.push_back(b);
-    x_p.push_back(b);
-    x_p.push_back(b);
+    Eigen::Vector3d a1 = {2.0, -1.0, 3.0};
+    Eigen::Vector3d a2 = {2.0, -1.0, 0.0};
+    Eigen::Vector3d a3 = {0.0, -1.0, 3.0};
+    Eigen::Vector3d a4 = {0.0, -1.0, 0.0};
+    Eigen::Vector3d a5 = {2.0, 0.0, 3.0};
+    Eigen::Vector3d a6 = {2.0, 0.0, 0.0};
+    
+    
+    Eigen::Vector3d b1 = {2.0, 1.0, 3.0};
+    Eigen::Vector3d b2 = {2.0, 1.0, 0.0};
+    Eigen::Vector3d b3 = {0.0, 1.0, 3.0};
+    Eigen::Vector3d b4 = {0.0, 1.0, 0.0};
+    Eigen::Vector3d b5 = {2.0, 0.0, 3.0};
+    Eigen::Vector3d b6 = {2.0, 0.0, 0.0};
+
+    x.push_back(a1);
+    x.push_back(a2);
+    x.push_back(a3);
+    x.push_back(a4);
+    x.push_back(a5);
+    x.push_back(a6);
+
+    x_p.push_back(b1);
+    x_p.push_back(b2);
+    x_p.push_back(b3);
+    x_p.push_back(b4);
+    x_p.push_back(b5);
+    x_p.push_back(b6);
 
     // 重み
     double weight = 1.0;
@@ -87,9 +128,19 @@ int main()
     Eigen::Matrix3d correlation_C;
     correlation_C = calc_correlation_C(x, x_p, weight);
 
+    std::cout << "correlation_C : " << std::endl;
     std::cout << correlation_C << std::endl;
 
-    SVD_correlation_C(correlation_C);    
+    // SVD_test(correlation_C);    
+    Eigen::Matrix3d matrix_R;
+    matrix_R = calc_rotation_R(correlation_C);
+
+
+    std::cout << "matrix_R :" << std::endl;
+    std::cout <<  matrix_R << std::endl; 
+    
+    std::cout << "x1_prime : " << std::endl << b2 << std::endl; 
+    std::cout << "R * x1 : " << std::endl << matrix_R * a2 << std::endl; 
 
     return 0;
 }
