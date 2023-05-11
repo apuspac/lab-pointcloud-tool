4626e33fb0e60c4e5d5ec69c01bf1d925096dff2
apuspac <workapus@gmail.com>
2023-05-11 17:54:42 +0900

    move

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "object_io.hpp"
 
 #include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+#include <cstdio>
 
 int main(int argc, char *argv[])
 {
@@ -15,23 +17,29 @@ int main(int argc, char *argv[])
     PointOperation opt;
     ObjectIO::option_process(argc, argv, opt);
     opt.print();
-    opt.mode_select();
+    // opt.mode_select();
 
-    // std::string bbox_info = "{\"xmin\":1583.7875976562,"
-    //                         "\"ymin\":1327.7779541016,"
-    //                         "\"xmax\":1835.1019287109,"
-    //                         "\"ymax\":1665.6372070312,"
-    //                         "\"confidence\":0.7136921287,"
-    //                         "\"class\":9,"
-    //                         "\"name\":\"square_box_w_marker_front_side\"}";
+    FILE *fp = fopen("detection.json", "r");
+    char readBuffer[65536];
+    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
 
-    // rapidjson::Document doc;
-    // doc.Parse(bbox_info.c_str());
+    rapidjson::Document
 
-    // std::cout << doc["xmin"].GetDouble() << std::endl;
-    // std::cout << doc["ymin"].GetDouble() << std::endl;
-    // std::cout << doc["name"].GetString() << std::endl;
-    // std::cout << doc["class"].GetInt() << std::endl;
+        std::string bbox_info = "{\"xmin\":1583.7875976562,"
+                                "\"ymin\":1327.7779541016,"
+                                "\"xmax\":1835.1019287109,"
+                                "\"ymax\":1665.6372070312,"
+                                "\"confidence\":0.7136921287,"
+                                "\"class\":9,"
+                                "\"name\":\"square_box_w_marker_front_side\"}";
+
+    rapidjson::Document doc;
+    doc.Parse(bbox_info.c_str());
+
+    std::cout << doc["xmin"].GetDouble() << std::endl;
+    std::cout << doc["ymin"].GetDouble() << std::endl;
+    std::cout << doc["name"].GetString() << std::endl;
+    std::cout << doc["class"].GetInt() << std::endl;
 
     return 0;
 }
