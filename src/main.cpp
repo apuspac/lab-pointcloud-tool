85524f4dc3a1990c056acaed947a877ce1a3ddc1
apuspac <workapus@gmail.com>
2023-05-22 14:41:31 +0900

    feat:#30 jsonfile load

--- src/main.cpp
+++ src/main.cpp
@@ -19,27 +19,39 @@ int main(int argc, char *argv[])
     opt.print();
     // opt.mode_select();
 
-    FILE *fp = fopen("detection.json", "r");
-    char readBuffer[65536];
-    rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    // jsonファイル読みこみ
+    const std::string jsonFilePath = "../../data/detection.json";
 
-    rapidjson::Document
+    std::ifstream jsonFile(jsonFilePath);
+    if (!jsonFile.is_open())
+    {
+        std::cerr << "Failed to open the jsonfile" << std::endl;
+        return 1;
+    }
 
-        std::string bbox_info = "{\"xmin\":1583.7875976562,"
-                                "\"ymin\":1327.7779541016,"
-                                "\"xmax\":1835.1019287109,"
-                                "\"ymax\":1665.6372070312,"
-                                "\"confidence\":0.7136921287,"
-                                "\"class\":9,"
-                                "\"name\":\"square_box_w_marker_front_side\"}";
+    // ファイル内容を文字列として読み込む
+    std::string jsonContent(
+        std::istreambuf_iterator<char>(jsonFile),
+        std::istreambuf_iterator<char>());
 
+    // jsonファイルを閉じる
+    jsonFile.close();
+
+    // パース
     rapidjson::Document doc;
-    doc.Parse(bbox_info.c_str());
+    doc.Parse(jsonContent.c_str());
 
-    std::cout << doc["xmin"].GetDouble() << std::endl;
-    std::cout << doc["ymin"].GetDouble() << std::endl;
-    std::cout << doc["name"].GetString() << std::endl;
-    std::cout << doc["class"].GetInt() << std::endl;
+    // errcheck
+    if (doc.HasParseError())
+    {
+        std::cerr << "Failed to parse the JSON content." << std::endl;
+        return 1;
+    }
+
+    // std::cout << doc["xmin"].GetDouble() << std::endl;
+    // std::cout << doc["ymin"].GetDouble() << std::endl;
+    std::cout << doc["inputpath"].GetString() << std::endl;
+    // std::cout << doc["class"].GetInt() << std::endl;
 
     return 0;
 }
